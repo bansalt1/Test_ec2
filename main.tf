b69e101d9486cf5268a84793dd4ab478c2672fbe
bansalt1 <125238093+bansalt1@users.noreply.github.com>
2023-07-12 13:46:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-    region = "ap-south-1"
-   access_key = "ASIAUWVAZLGW5IOBWY4G"
-  secret_key = "A4RW7g2zIuQikxNjHDe/7FJDh2zo/V/XEv14Jpty"               
-}
-resource "aws_iam_user" "tarun" {
-    name  = "tarun-test"
+resource "null_resource" "test" {
+
 }
